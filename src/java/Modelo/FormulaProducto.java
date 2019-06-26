1950fd109fdcd0d9066e44526dc30987dee451b2
WilliamLop <calderonwilliam25@gmail.com>
2019-06-26 18:35:26 -0500

    Paquete modelo listo

---
+++ src/java/Modelo/FormulaProducto.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class FormulaProducto {
+    private int idformproducto;
+    private String dosis;
+    private Formula idformula;
+
+    public int getIdformproducto() {
+        return idformproducto;
+    }
+
+    public void setIdformproducto(int idformproducto) {
+        this.idformproducto = idformproducto;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public Formula getIdformula() {
+        return idformula;
+    }
+
+    public void setIdformula(Formula idformula) {
+        this.idformula = idformula;
+    }
+
+    
+    
+}
